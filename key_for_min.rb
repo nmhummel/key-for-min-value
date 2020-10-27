e6f764139506b8bc5e0c62d39e2b06721b620c0f
nmhummel <natalie.hummel@gmail.com>
2020-10-26 19:25:41 -0500

    commit tree 895b02aa7d9f869f3ce174b25e2323c6ad767e23

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+#  hash = {:blake => 500, :ashley => 2, :adam => -3, :joyce => 0}
 
 def key_for_min_value(name_hash)
-
+    num = 1.0 / 0
+    tiny_person = nil
+    name_hash.each do |key, value|  #500, 2, -3
+        if num == 0 #infinity == 0                      2 == 0                  -3 == 0                 0 == 0
+            num = value # skip                          skip                    skip                    num = 0
+            tiny_person = key #skip                     skip                    skip                    tiny_person = joyce
+        elsif value < num #500 < infinity               2 < 500                 -3 < 2                  skip
+            num = value #num = 500                      num = 2                 num = -3                skip
+            tiny_person = key #tiny_person = blake      tiny_person = ashley    tiny_person = adam      skip
+        end
+    end
+    return tiny_person #adam
+    if name_hash.empty?
+        return nil  
+    end
 end
